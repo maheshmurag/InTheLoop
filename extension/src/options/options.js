62eeb1e61b23b45a4be94527f486559dee774e1b
Mahesh Murag <maheshmmurag@gmail.com>
2016-01-03 23:32:33 -0800

    reverted accidental code changes

--- extension/src/options/options.js
+++ extension/src/options/options.js
@@ -179,7 +179,6 @@ var currentTheme = "none";
 var dropdown = document.getElementById("theme_select");
 
 function loadThemes(){
-    ga();
 	chrome.storage.sync.get({current_theme:themes.none, custom_theme:custom_default},function(data){
 
 		//in case we've added more stuff, add saved info to our default custom
@@ -278,18 +277,6 @@ function loadSubdomain(){
 	chrome.storage.sync.get({sl_subdomain:"montavista"}, function(data){
 		slSubdomain.value = data.sl_subdomain;
 	});
-}
-
-function ga(){
-         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  
-    ga('create', 'UA-52774095-3', 'auto');
-    ga('set', 'checkProtocolTask', null);
-    ga('send', 'pageview', '/options.html');
-    alert("sdffffff")
 }
 
 function init(){
